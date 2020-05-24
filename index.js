1950cc09d1ec9b7657f0de5898241da3bb70e0b5
chapdel <chapdel.kamga2@gmail.com>
2020-05-24 23:34:25 +0100

    Adding method

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-module.exports = (str) => {
-    return `💩${str}💩`;
-}
+module.exports = function aesEncrypt(string) {
+    if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+    return string.replace(/\s/g, "");
+};
+
+module.exports = function aesDecrypt(string) {
+    if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+    return string.replace(/\s/g, "");
+};
